c639c64b9dde95533366899c71a357f7d778883d
syara11 <putrisyarasfina@gmail.com>
2024-10-25 11:24:01 +0700

    Pesan commit kamu

---
+++ src/DebugExample.java
@@ -0,0 +1,11 @@
+public class DebugExample {
+    //pvst
+    public static void main(String[] args){
+        int x = 5;
+        int y = 10;
+        int sum = x + y;// hasil penjumlahan
+
+        System.out.println("sum = " + sum );//menampilkan hasil penjumlahan
+    }
+}
+
